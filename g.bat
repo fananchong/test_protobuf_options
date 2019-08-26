5bdbe767f2e056270b3cb3ab8e63fc382d9a2d6b
fananchong <fananchong@126.com>
2019-08-26 18:38:03 +0800

    up

--- g.bat
+++ g.bat
@@ -1,8 +1,4 @@
-go list -m -f "{{.Dir}}" github.com/fananchong/test_protobuf_options > temp
-set /P DEP1=<temp
 go list -m -f "{{.Dir}}" github.com/golang/protobuf > temp
-set /P DEP2=<temp
-echo %DEP1%
-echo %DEP2%
-
-protoc -I. -I%DEP1% -I%DEP2% --go_out=. test.proto
+set /P DEP=<temp
+echo %DEP%
+protoc -I. -I%DEP% --go_out=. broadcast.proto
